d0626a974ba8a686c600c8cc206245985b2bf53f
Alessandro Barbieri <info@alessandrobarbieri.net>
2019-05-22 16:26:23 +0200

    Add Shader01

---
+++ components/shader-01.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Shader01 extends Component {
+  render(){
+    return <div>Shader 01</div>;
+  }
+}
+
+export default Shader01;
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Shader01 from "../components/shader-01";
+
+function Home() {
+  return <Shader01 />;
+}
+
+export default Home;
